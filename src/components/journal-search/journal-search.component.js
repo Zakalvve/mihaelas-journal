5c9c50c67bcf99bf84b4a528bb5cfada1231eabf
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-02-04 17:58:05 +0000

    simple edit

--- src/components/journal-search/journal-search.component.js
+++ src/components/journal-search/journal-search.component.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect } from "react";
+import React, {useState } from "react";
 import { searchResults } from "../../serverProxy";
 
 export const JournalSearch = (props) => {
@@ -6,7 +6,7 @@ export const JournalSearch = (props) => {
     //when inputString changes, update the available options
     const [selectOptions] = useState(searchResults(""));
     
-    
+
     const handleChange = ({target}) => {
         props.onChange(target.value);
     }
