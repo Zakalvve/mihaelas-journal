ca1aae68803f1d6d5e999305ef055cacff030302
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-02-05 13:19:15 +0000

    layout change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,16 @@ import { Link } from "react-router-dom";
 
 export const Home = () => {
     return (
-        <div className="mt-5">
+        <div className="pt-5 d-flex flex-column h-100">
             <h1 className="mb-3">Welcome to Mihaelas journal</h1>
 
             <p>This is a public place to browse the ups and downs of the rougish halflings adventures through the lands of Katya.</p>
 
             <p>This journal was written by Michael as a part of the ongoing DnD campaign A Matter of Blood.</p>
-
-            <Link to="/journal">Check out the first entry and go from there</Link>
+            
+            <div className="flex-grow-1 d-flex flex-column-reverse p-3 pb-5">
+                <Link to="/journal">Check out the first entry and go from there</Link>
+            </div>
         </div>
     );
 }
